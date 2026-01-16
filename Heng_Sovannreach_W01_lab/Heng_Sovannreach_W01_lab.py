966959566bf8b811abe2d0a0704460383601702a
HengSovannreach404 <sovannreach.heng@student.cadt.edu.kh>
2026-01-16 21:51:13 +0700

    Update full exercises of lab

--- Heng_Sovannreach_W01_lab/Heng_Sovannreach_W01_lab.py
+++ Heng_Sovannreach_W01_lab/Heng_Sovannreach_W01_lab.py
@@ -71,3 +71,12 @@
 '''
 
 string = input("Enter a string: ")
+is_palindrome = True
+for i in range( 0, len(string) ):
+      if string[i] != string[len(string) - i - 1]:
+                 is_palindrome = False
+
+if is_palindrome :
+    print(f"{string} is a palindrome")
+else :
+    print(f"{string} is not a palindrome")
